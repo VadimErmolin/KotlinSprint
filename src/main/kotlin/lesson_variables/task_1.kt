05aa0ecf28647535851ecf9102d20e925d62543b
Vadim <vaaadim.ermolin@mail.ru>
2025-08-01 14:52:04 +0300

    KS-1-1 do smth

---
+++ src/main/kotlin/lesson_variables/task_1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_variables
+fun main(){
+    val numberOfOrders = 75
+    val thankYouMessage = "Спасибо, что выбрали наш магазин"
+
+    println("Задача 1.1")
+    println("Количество заказов: $numberOfOrders")
+    println(thankYouMessage)
+    println("\nЗадача 1.2")
+
+    val sideOfRectangleA : Int = 10
+    val sideOfRectangleB : Int = 15
+
+    println("Сторона треугольника А = $sideOfRectangleA")
+    println("Сторона треугольника В = $sideOfRectangleB")
+
+}
